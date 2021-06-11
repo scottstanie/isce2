ece7f93ca6c47b5ed351a91fa551d35765729d6a
scott <scott.stanie@gmail.com>
2021-06-11 14:14:03 -0500

    format files

--- components/isceobj/Sensor/UAVSAR_Polsar.py
+++ components/isceobj/Sensor/UAVSAR_Polsar.py
@@ -1,18 +1,18 @@
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Copyright 2014 California Institute of Technology. ALL RIGHTS RESERVED.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# 
+#
 # United States Government Sponsorship acknowledged. This software is subject to
 # U.S. export control laws and regulations and has been classified as 'EAR99 NLR'
 # (No [Export] License Required except when exporting to an embargoed country,
@@ -23,13 +23,13 @@
 # embargoed foreign country or citizen of those countries.
 #
 # Authors: Marco Lavalle, Eric Gurrola
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-
-
-
-from __future__ import (print_function, absolute_import,)
+from __future__ import (
+    print_function,
+    absolute_import,
+)
 import datetime
 import math
 import numpy
@@ -46,21 +46,31 @@ import os
 from isceobj.Util.py2to3 import *
 
 METADATAFILE = Component.Parameter(
-    'metadataFile',
-    public_name='annotation file',
+    "metadataFile",
+    public_name="annotation file",
     default=None,
     type=str,
     mandatory=True,
-    doc="Name of the input annotation file"
+    doc="Name of the input annotation file",
 )
 
 OUTPUT = Component.Parameter(
-    'output',
-    public_name='OUTPUT',
-    default='',
+    "output",
+    public_name="OUTPUT",
+    default="",
     type=str,
     mandatory=False,
-    doc="Name of output slc file"
+    doc="Name of output slc file",
+)
+
+POLARIZATION = Component.Parameter(
+    "polarization",
+    public_name="POLARIZATION",
+    default="HH",
+    type=str,
+    mandatory=True,
+    intent="input",
+    doc="polarization channel of the UAVSAR slc file to be processed",
 )
 
 
@@ -69,69 +79,64 @@ class UAVSAR_Polsar(Sensor):
     A class representing a UAVSAR Polsar SLC.
     """
 
-    family = 'uavsar_polsar'
-    logging_name = 'isce.Sensor.UAVSAR_Polsar'
-    lookMap = {'RIGHT': -1,
-               'LEFT': 1}
+    family = "uavsar_polsar"
+    logging_name = "isce.Sensor.UAVSAR_Polsar"
+    lookMap = {"RIGHT": -1, "LEFT": 1}
 
     parameter_list = (METADATAFILE,) + Sensor.parameter_list
 
-    def __init__(self, name=''):
+    def __init__(self, name=""):
         super().__init__(family=self.family, name=name)
         self.frame = Frame()
         self.frame.configure()
         self._elp = None
         self._peg = None
-        
+
     def _populatePlatform(self, **kwargs):
         platform = self.frame.getInstrument().getPlatform()
-        platform.setMission('UAVSAR')
+        platform.setMission("UAVSAR")
         platform.setPointingDirection(
-            self.lookMap[self.metadata['Look Direction'].upper()])
+            self.lookMap[self.metadata["Look Direction"].upper()]
+        )
         platform.setPlanet(Planet(pname="Earth"))
         platform.setAntennaLength(1.5)
 
     def _populateInstrument(self, **kwargs):
         fudgefactor = 1.0  # 1.0/1.0735059946800756
         instrument = self.frame.getInstrument()
-        instrument.setRadarWavelength(
-            self.metadata['Center Wavelength'])
+        instrument.setRadarWavelength(self.metadata["Center Wavelength"])
         instrument.setPulseRepetitionFrequency(
-            fudgefactor*1.0/self.metadata['Average Pulse Repetition Interval'])
-        instrument.setRangePixelSize(
-            self.metadata['slc_mag.col_mult'])
-        instrument.setAzimuthPixelSize(
-            self.metadata['slc_mag.row_mult'])
-        instrument.setPulseLength(
-            self.metadata['Pulse Length'])
+            fudgefactor * 1.0 / self.metadata["Average Pulse Repetition Interval"]
+        )
+        instrument.setRangePixelSize(self.metadata["slc_mag.col_mult"])
+        instrument.setAzimuthPixelSize(self.metadata["slc_mag.row_mult"])
+        instrument.setPulseLength(self.metadata["Pulse Length"])
         instrument.setChirpSlope(
-            -self.metadata['Bandwidth'] / self.metadata['Pulse Length'])
+            -self.metadata["Bandwidth"] / self.metadata["Pulse Length"]
+        )
         instrument.setRangeSamplingRate(
-            SPEED_OF_LIGHT / 2.0 / instrument.getRangePixelSize())
+            SPEED_OF_LIGHT / 2.0 / instrument.getRangePixelSize()
+        )
 
     def _populateFrame(self, **kwargs):
         tStart = datetime.datetime.strptime(
-            self.metadata['Start Time of Acquisition'],
-            "%d-%b-%Y %H:%M:%S %Z"
+            self.metadata["Start Time of Acquisition"], "%d-%b-%Y %H:%M:%S %Z"
         )
         tStop = datetime.datetime.strptime(
-            self.metadata['Stop Time of Acquisition'],
-            "%d-%b-%Y %H:%M:%S %Z"
+            self.metadata["Stop Time of Acquisition"], "%d-%b-%Y %H:%M:%S %Z"
         )
-        dtMid = DTU.timeDeltaToSeconds(tStop - tStart)/2.
-        tMid = tStart + datetime.timedelta(microseconds=int(dtMid*1e6))
+        dtMid = DTU.timeDeltaToSeconds(tStop - tStart) / 2.0
+        tMid = tStart + datetime.timedelta(microseconds=int(dtMid * 1e6))
 
         frame = self.frame
         frame.setSensingStart(tStart)
         frame.setSensingStop(tStop)
         frame.setSensingMid(tMid)
-        frame.setNumberOfLines(
-            int(self.metadata['slc_mag.set_rows']))
-        frame.setNumberOfSamples(
-            int(self.metadata['slc_mag.set_cols']))
-        
-        frame.C0 = self.metadata['slc_mag.col_addr']
-        frame.S0 = self.metadata['slc_mag.row_addr']
+        frame.setNumberOfLines(int(self.metadata["slc_mag.set_rows"]))
+        frame.setNumberOfSamples(int(self.metadata["slc_mag.set_cols"]))
+
+        frame.C0 = self.metadata["slc_mag.col_addr"]
+        frame.S0 = self.metadata["slc_mag.row_addr"]
 
         self.extractDoppler()
         frame.setStartingRange(self.startingRange)
@@ -140,8 +145,8 @@ class UAVSAR_Polsar(Sensor):
         width = frame.getNumberOfSamples()
         deltaRange = frame.instrument.getRangePixelSize()
         nearRange = frame.getStartingRange()
-        
-        frame.setFarRange(nearRange+width*deltaRange)
+
+        frame.setFarRange(nearRange + width * deltaRange)
 
         frame._ellipsoid = self.elp
         frame.peg = self.peg
@@ -150,27 +155,35 @@ class UAVSAR_Polsar(Sensor):
         frame.terrainHeight = self.terrainHeight
         frame.upperLeftCorner = Coordinate()
         frame.upperLeftCorner.setLatitude(
-            math.degrees(self.metadata['Approximate Upper Left Latitude']))
+            math.degrees(self.metadata["Approximate Upper Left Latitude"])
+        )
         frame.upperLeftCorner.setLongitude(
-            math.degrees(self.metadata['Approximate Upper Left Longitude']))
+            math.degrees(self.metadata["Approximate Upper Left Longitude"])
+        )
         frame.upperLeftCorner.setHeight(self.terrainHeight)
         frame.upperRightCorner = Coordinate()
         frame.upperRightCorner.setLatitude(
-            math.degrees(self.metadata['Approximate Upper Right Latitude']))
+            math.degrees(self.metadata["Approximate Upper Right Latitude"])
+        )
         frame.upperRightCorner.setLongitude(
-            math.degrees(self.metadata['Approximate Upper Right Longitude']))
+            math.degrees(self.metadata["Approximate Upper Right Longitude"])
+        )
         frame.upperRightCorner.setHeight(self.terrainHeight)
         frame.lowerRightCorner = Coordinate()
         frame.lowerRightCorner.setLatitude(
-            math.degrees(self.metadata['Approximate Lower Right Latitude']))
+            math.degrees(self.metadata["Approximate Lower Right Latitude"])
+        )
         frame.lowerRightCorner.setLongitude(
-            math.degrees(self.metadata['Approximate Lower Right Longitude']))
+            math.degrees(self.metadata["Approximate Lower Right Longitude"])
+        )
         frame.lowerRightCorner.setHeight(self.terrainHeight)
         frame.lowerLeftCorner = Coordinate()
         frame.lowerLeftCorner.setLatitude(
-            math.degrees(self.metadata['Approximate Lower Left Latitude']))
+            math.degrees(self.metadata["Approximate Lower Left Latitude"])
+        )
         frame.lowerLeftCorner.setLongitude(
-            math.degrees(self.metadata['Approximate Lower Left Longitude']))
+            math.degrees(self.metadata["Approximate Lower Left Longitude"])
+        )
         frame.lowerLeftCorner.setHeight(self.terrainHeight)
 
     def _populateFrameSolo(self):
@@ -195,24 +208,26 @@ class UAVSAR_Polsar(Sensor):
         # make sure the elp property has been called
         elp = self.elp
         orbit = self.frame.getOrbit()
-        orbit.setOrbitSource('Header')
+        orbit.setOrbitSource("Header")
 
-        for i in range(-5*numgroup, int(nlines/numgroup)*numgroup+5*numgroup, numgroup):
+        for i in range(
+            -5 * numgroup, int(nlines / numgroup) * numgroup + 5 * numgroup, numgroup
+        ):
             delt = int(i * 1.0e6 / prf)
             torb = self.frame.getSensingStart() + datetime.timedelta(microseconds=delt)
-            ds = delt*1.0e-6*vel
+            ds = delt * 1.0e-6 * vel
 
             vec = OrbitStateVector()
 
             posSCH = [self.frame.S0 + ds, 0.0, self.platformHeight]
-            velSCH = [self.velocity, 0., 0.]
+            velSCH = [self.velocity, 0.0, 0.0]
             posXYZ, velXYZ = elp.schdot_to_xyzdot(posSCH, velSCH)
 
             vec.setTime(torb)
             vec.setPosition(posXYZ)
             vec.setVelocity(velXYZ)
             orbit.addStateVector(vec)
-            
+
     def populateMetadata(self):
         self._populatePlatform()
         self._populateInstrument()
@@ -221,17 +236,19 @@ class UAVSAR_Polsar(Sensor):
 
     def extractImage(self):
         from iscesys.Parsers import rdf
+
         self.metadata = rdf.parse(self.metadataFile)
         self.populateMetadata()
         slcImage = isceobj.createSlcImage()
         self.slcname = os.path.join(
             os.path.dirname(os.path.abspath(self.metadataFile)),
-            self.metadata['slc'+self.polarization.upper()])
+            self.metadata["slc" + self.polarization.upper()],
+        )
         slcImage.setFilename(self.slcname)
         slcImage.setXmin(0)
         slcImage.setXmax(self.frame.getNumberOfSamples())
         slcImage.setWidth(self.frame.getNumberOfSamples())
-        slcImage.setAccessMode('r')
+        slcImage.setAccessMode("r")
         slcImage.renderHdr()
         self.frame.setImage(slcImage)
 
@@ -243,14 +260,15 @@ class UAVSAR_Polsar(Sensor):
         # do the unit conversion properly.
 
         # The units, and values parsed from the metadataFile
-        key = 'Reskew Doppler Near Mid Far'
-        u = self.metadata.data[key].units.split(',')
+        key = "Reskew Doppler Near Mid Far"
+        u = self.metadata.data[key].units.split(",")
         v = map(float, self.metadata.data[key].value.split())
-        k = ["Reskew Doppler "+x for x in ("Near", "Mid", "Far")]
+        k = ["Reskew Doppler " + x for x in ("Near", "Mid", "Far")]
 
         # Use the interactive RDF accumulator to create an RDF object
         # for the near, mid, and far Doppler values
         from iscesys.Parsers.rdf import iRDF
+
         dop = iRDF.RDFAccumulator()
         for z in zip(k, u, v):
             dop("%s (%s) = %f" % z)
@@ -264,37 +282,41 @@ class UAVSAR_Polsar(Sensor):
         frame = self.frame
         instrument = frame.getInstrument()
         width = frame.getNumberOfSamples()
-        nearRangeBin = 0.
-        midRangeBin = float(int((width-1.0)/2.0))
-        farRangeBin = width-1.0
+        nearRangeBin = 0.0
+        midRangeBin = float(int((width - 1.0) / 2.0))
+        farRangeBin = width - 1.0
 
-        A = numpy.matrix([[1.0, nearRangeBin, nearRangeBin**2],
-                          [1.0, midRangeBin,  midRangeBin**2],
-                          [1.0, farRangeBin,  farRangeBin**2]])
-        d = numpy.matrix([self.dopplerVals['Near'],
-                          self.dopplerVals['Mid'],
-                          self.dopplerVals['Far']]).transpose()
-        coefs = (numpy.linalg.inv(A)*d).transpose().tolist()[0]
+        A = numpy.matrix(
+            [
+                [1.0, nearRangeBin, nearRangeBin ** 2],
+                [1.0, midRangeBin, midRangeBin ** 2],
+                [1.0, farRangeBin, farRangeBin ** 2],
+            ]
+        )
+        d = numpy.matrix(
+            [self.dopplerVals["Near"], self.dopplerVals["Mid"], self.dopplerVals["Far"]]
+        ).transpose()
+        coefs = (numpy.linalg.inv(A) * d).transpose().tolist()[0]
         prf = instrument.getPulseRepetitionFrequency()
-        coefs_norm = {'a': coefs[0]/prf,
-                      'b': coefs[1]/prf,
-                      'c': coefs[2]/prf}
+        coefs_norm = {"a": coefs[0] / prf, "b": coefs[1] / prf, "c": coefs[2] / prf}
 
         self.doppler_coeff = coefs
         return coefs_norm
-    
+
     @property
     def terrainHeight(self):
         # The peg point incorporates the actual terrainHeight
         # return self.metadata['Global Average Terrain Height']
         return 0.0
-        
+
     @property
     def platformHeight(self):
         # Reduce the platform height by the terrain height because the
         # peg radius of curvature includes the terrain height
-        h = (self.metadata['Global Average Altitude'] -
-             self.metadata['Global Average Terrain Height'])
+        h = (
+            self.metadata["Global Average Altitude"]
+            - self.metadata["Global Average Terrain Height"]
+        )
         return h
 
     @property
@@ -304,7 +326,7 @@ class UAVSAR_Polsar(Sensor):
 
     @property
     def startingRange(self):
-        return self.metadata['Image Starting Range']
+        return self.metadata["Image Starting Range"]
 
     @property
     def squintAngle(self):
@@ -317,22 +339,19 @@ class UAVSAR_Polsar(Sensor):
         wavelength = self.frame.getInstrument().getRadarWavelength()
 
         if h > startingRange:
-            raise ValueError("Spacecraft Height too large (%s>%s)" %
-                             (h, startingRange))
+            raise ValueError("Spacecraft Height too large (%s>%s)" % (h, startingRange))
 
-        sinTheta = math.sqrt(1 - (h/startingRange)**2)
-        fd = self.dopplerVals['Near']
-        sinSquint = fd/(2.0*v*sinTheta)*wavelength
+        sinTheta = math.sqrt(1 - (h / startingRange) ** 2)
+        fd = self.dopplerVals["Near"]
+        sinSquint = fd / (2.0 * v * sinTheta) * wavelength
 
-        if sinSquint**2 > 1:
+        if sinSquint ** 2 > 1:
             raise ValueError(
-                "Error in One or More of the Squint Calculation Values\n" +
-                "Doppler Centroid: %s\nVelocity: %s\nWavelength: %s\n" %
-                (fd, v, wavelength)
-                )
-        self.squint = math.degrees(
-            math.atan2(sinSquint, math.sqrt(1-sinSquint**2))
+                "Error in One or More of the Squint Calculation Values\n"
+                + "Doppler Centroid: %s\nVelocity: %s\nWavelength: %s\n"
+                % (fd, v, wavelength)
             )
+        self.squint = math.degrees(math.atan2(sinSquint, math.sqrt(1 - sinSquint ** 2)))
         # jng squint is also used later on from the frame, just add it here
         self.frame.squintAngle = math.radians(self.squint)
 
@@ -351,10 +370,10 @@ class UAVSAR_Polsar(Sensor):
         elp = platform.getPlanet().get_elp()
         peg = self.peg
         elp.setSCH(peg.latitude, peg.longitude, peg.heading)
-        scale = (elp.pegRadCur + self.platformHeight)/elp.pegRadCur
+        scale = (elp.pegRadCur + self.platformHeight) / elp.pegRadCur
         ds_ground = self.frame.instrument.getAzimuthPixelSize()
-        dt = 1.0/self.frame.instrument.getPulseRepetitionFrequency()
-        v = scale*ds_ground/dt
+        dt = 1.0 / self.frame.instrument.getPulseRepetitionFrequency()
+        v = scale * ds_ground / dt
         return v
 
     @property
@@ -367,12 +386,14 @@ class UAVSAR_Polsar(Sensor):
     @property
     def peg(self):
         if not self._peg:
-            peg = [math.degrees(self.metadata['Peg Latitude']),
-                   math.degrees(self.metadata['Peg Longitude']),
-                   math.degrees(self.metadata['Peg Heading'])]
-            th = self.metadata['Global Average Terrain Height']
+            peg = [
+                math.degrees(self.metadata["Peg Latitude"]),
+                math.degrees(self.metadata["Peg Longitude"]),
+                math.degrees(self.metadata["Peg Heading"]),
+            ]
+            th = self.metadata["Global Average Terrain Height"]
 
-            if self.metadata['Mocomp II Applied'] is 'Y':
+            if self.metadata["Mocomp II Applied"] is "Y":
                 self.elp.setSCH(peg[0], peg[1], peg[2], th)
             else:
                 self.elp.setSCH(peg[0], peg[1], peg[2], 0)
@@ -380,11 +401,19 @@ class UAVSAR_Polsar(Sensor):
             rc = self.elp.pegRadCur
 
             from isceobj.Location.Peg import Peg
-            self._peg = Peg(latitude=peg[0], longitude=peg[1], heading=peg[2],
-                            radiusOfCurvature=rc)
 
-            self.logger.info("UAVSAR_Polsar: peg radius of curvature = {}".format(self.elp.pegRadCur))
+            self._peg = Peg(
+                latitude=peg[0], longitude=peg[1], heading=peg[2], radiusOfCurvature=rc
+            )
+
+            self.logger.info(
+                "UAVSAR_Polsar: peg radius of curvature = {}".format(self.elp.pegRadCur)
+            )
             self.logger.info("UAVSAR_Polsar: terrain height = {}".format(th))
-            self.logger.info("UAVSAR_Polsar: mocomp II applied = {}".format(self.metadata['Mocomp II Applied']))
+            self.logger.info(
+                "UAVSAR_Polsar: mocomp II applied = {}".format(
+                    self.metadata["Mocomp II Applied"]
+                )
+            )
 
         return self._peg
